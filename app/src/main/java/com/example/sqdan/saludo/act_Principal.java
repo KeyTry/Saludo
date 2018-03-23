40f3320a32604ff5afb8707e640145a341ba650d
Daniel Somarribas <sq.daniel@gmail.com>
2018-03-22 18:26:55 -0600

    Initial commit

---
+++ app/src/main/java/com/example/sqdan/saludo/act_Principal.java
@@ -0,0 +1,13 @@
+package com.example.sqdan.saludo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class act_Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lyt_principal);
+    }
+}
