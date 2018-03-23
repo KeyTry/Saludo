59da83df81db11e5bb2e6b32dee928c108e34f0d
Daniel Somarribas <sq.daniel@gmail.com>
2018-03-22 21:01:01 -0600

    Basic app

--- app/src/main/java/com/example/sqdan/saludo/act_Principal.java
+++ app/src/main/java/com/example/sqdan/saludo/act_Principal.java
@@ -2,12 +2,29 @@ package com.example.sqdan.saludo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class act_Principal extends AppCompatActivity {
+
+    EditText txtNombre;
+    Button btnAceptar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lyt_principal);
+        txtNombre = (EditText)findViewById(R.id.txtNombre);
+        btnAceptar = (Button)findViewById(R.id.btnAceptar);
+
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre = txtNombre.getText().toString();
+                Toast.makeText(getApplicationContext(),"Hola, "+nombre,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
